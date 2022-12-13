5303852ff7563d9a76359c907613df0c19fc752b
aviulsler <trashfuertrash@gmail.com>
2022-12-13 13:27:09 +0100

    correction in tail.php

--- php/tail.php
+++ php/tail.php
@@ -2,7 +2,7 @@
 
   if ( $nbrow == '0' ) {
     print "<h1>Sie sind nicht angemeldet</h1>\n";
-    print "<meta http-equiv=\"refresh\" content=\"5; url=.\">\n";
+    print "<meta http-equiv=\"refresh\" content=\"5; url=../index.html\">\n";
   }
 print "</html>\n";
 ?>
